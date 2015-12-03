fb5858ba77113b00f6a0e8f7ecf2f1e0ab3acd7b
Gene McCullagh <gene@lightroomsecrets.com>
2015-12-03 05:00:03 -0600

    login screen completed

---
+++ MaterialTextField.swift
@@ -0,0 +1,28 @@
+//
+//  MaterialTextField.swift
+//  lrs-showcase
+//
+//  Created by Gene McCullagh on 12/2/15.
+//  Copyright © 2015 Gene McCullagh. All rights reserved.
+//
+
+import UIKit
+
+class MaterialTextField: UITextField {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.borderColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.1).CGColor
+        layer.borderWidth = 1.0
+    }
+    
+    //for placeholder
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+
+}
---
+++ lrs-showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  lrs-showcase
+//
+//  Created by Gene McCullagh on 12/2/15.
+//  Copyright © 2015 Gene McCullagh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
