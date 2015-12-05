dcbc53d21bf6a8c4e4150b71e8e6a59ab8b40aaa
Gene McCullagh <gene@lightroomsecrets.com>
2015-12-05 06:12:10 -0600

    Main post interface layout done

---
+++ FeedVC.swift
@@ -0,0 +1,36 @@
+//
+//  FeedVC.swift
+//  lrs-showcase
+//
+//  Created by Gene McCullagh on 12/4/15.
+//  Copyright © 2015 Gene McCullagh. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+
+
+}
---
+++ PostCell.swift
@@ -0,0 +1,34 @@
+//
+//  PostCell.swift
+//  lrs-showcase
+//
+//  Created by Gene McCullagh on 12/4/15.
+//  Copyright © 2015 Gene McCullagh. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
